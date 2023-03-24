5c611035da4c32bf674fc286b56af9a5baa619b6
Brian <bbarr0101@gmail.com>
2023-03-24 10:45:06 -0700

    creates fallback point for global css edits

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,30 +1,44 @@
 <template>
-    <Header />
-    <h1>product page</h1>
-    <p>{{ $route.params.id }}</p>
-    <Footer />
-  </template>
+  <Header />
+    <main>
+      <h2>{{ product.title }}</h2>
+      <img :src="product.thumbnail" alt="product">
+      <pre>{{ product }}</pre>
+      <p>{{ product.description}}</p>
+    </main>
+
+  <Footer />
+</template>
   
 <script lang="ts" setup>
     import Header from '@/components/SiteHeader.vue'
     import Footer from '@/components/SiteFooter.vue'
     import type Product from '@/types/product';
     import { useRoute } from 'vue-router';
-    import { computed } from '@vue/reactivity';
+import { ref } from 'vue';
 
 
     const route = useRoute()
     // need to cast the paramater; this caused such a god damn fit
     let id:number = parseInt(<string>route.params.id);
-
-    
+   
+    let product = ref();
     console.log(id);
     console.log(typeof(id));
-    
-    const response = await fetch(`https://dummyjson.com/products/${id}`);
-    const product:Product = await response.json()
-  
+
+    try {
+      const response = await fetch(`https://dummyjson.com/products/${id}`);
+      product= await response.json();
+    } catch (error) {
+        console.log(error); 
+    }
+
+
 </script>
   
-  <style lang="scss" scoped></style>
+<style lang="css" scoped>
+  main{
+    border: 2px solid blue;
+  }
+</style>
   
