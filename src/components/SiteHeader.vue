82d4e6a1c9c479bba938ebb7d39212d4fbe2782e
Brian <bbarr0101@gmail.com>
2023-04-03 08:43:57 -0700

    adds search bar to the site

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -15,6 +15,15 @@
       </ul>
     </nav>
   </header>
+  <div class="searchbar">
+    <input v-if="displaySearch" class="search-input" type="text" placeholder="search">
+    <button @click="showSearch">
+      <span>
+        <font-awesome-icon v-if="!displaySearch" :icon="['fas', 'angles-down']" />
+        <font-awesome-icon v-else :icon="['fas', 'angles-up']" />
+      </span>
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,6 +31,10 @@ import { RouterLink } from 'vue-router';
 
 import { ref } from 'vue'
 const title = ref('The Proto Shop')
+const displaySearch = ref(false); 
+const showSearch = () => {displaySearch.value = !displaySearch.value; console.log(displaySearch.value);
+}
+
 </script>
 
 <style scoped>
@@ -47,5 +60,28 @@ header {
 
 .nav-item{
   font-size: 1.75rem;
+}
+
+.searchbar{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.search-input{
+  width: 100%;
+  max-width: 1280px;
+  line-height: 1.5rem;
+  font-size: 1.25rem;
+}
+
+span{
+  color: var(--accent-clr);
+  max-width: 20px;
+  font-size: 1.25rem;
+}
+
+button{
+  border: none;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faTwitter, faInstagram,faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons'
+import {faAngleDoubleDown, faAngleDoubleUp} from '@fortawesome/free-solid-svg-icons'
 import './assets/main.css'
 
-library.add( faTwitter, faInstagram,faFacebook, faGithub); 
+library.add( faTwitter, faInstagram,faFacebook, faGithub, faAngleDoubleDown, faAngleDoubleUp); 
 const app = createApp(App)
 
 app.use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
