58d147366de1dc80dafe17e8db792bd7898ad6bc
blbarr01 <bbarr0101@gmail.com>
2023-04-13 12:02:52 -0700

    adds scaffolding for a category select component

---
+++ src/components/CategorySelector.vue
@@ -0,0 +1,13 @@
+<template lang="en">
+    <select name= "categories" id="category-selector" v-model="selected"> 
+
+    </select>            
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="">
+    
+</style>
