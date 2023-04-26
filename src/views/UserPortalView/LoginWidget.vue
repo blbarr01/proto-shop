9e03c40f441bff40522b3956ddd7f46d75bb1fc2
Brian <bbarr0101@gmail.com>
2023-04-26 09:12:28 -0700

    adds redirect for successful login

--- src/views/UserPortalView/LoginWidget.vue
+++ src/views/UserPortalView/LoginWidget.vue
@@ -12,24 +12,43 @@
           />
 
     </form>
+    <div v-if="errorMsg" class="error-msg">
+      <p>{{ errorMsg }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
     import {ref} from 'vue'
     import { supabase } from '@/supabase'
+    import { useRouter } from 'vue-router'
+    
+
     const loading = ref(false)
     const password = ref('')
     const email = ref('')
+    const errorMsg = ref('')
+    const router = useRouter()
+
 
     async function handleLogin(){
       try{
         let { data, error } = await supabase.auth.signInWithPassword({
-        email: email.value,
-        password: password.value
-    })
+          email: email.value,
+          password: password.value
+        })
+        if(error){
+          errorMsg.value = error.message
+          setTimeout(()=>errorMsg.value ='',5000)
+          throw error
+        }
+        console.log(data);
+        const user  = data.user
+        console.log(user);
+        
+        router.push({name: "Account"})
       }catch(error){
-
+        console.error(error);
       }
     }
 
