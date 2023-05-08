efe5cf20d96fb0daa22f62b3fda3432e3107c1fb
Brian <bbarr0101@gmail.com>
2023-05-08 16:08:58 -0700

    refractors profile cards into their own component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,22 +1,13 @@
 <template>
   <Header />
-
+  <main>
   <div class="crew">
     <h2>Meet the crew!</h2>
   </div>
-
   <div class="container">
-    <div class="team-wrapper" v-for="member in team" :key="member.name">
-      <img :src="member.image" class="img-responsive" alt="team img">
-      <h3>{{ member.name }}</h3>
-      <h4>{{ member.role }}</h4>
-      <p>Computer Sience graduate</p>
-      <p>California State University - Northridge</p>
-      <a :href="member.github_link"> Github</a>
-      <a :href="member.linkedin_link"> LinkedIn</a>
-    </div>
+      <ProfileCard v-for="profile in team" :profile="profile"/>
   </div>
-
+</main>
   <Footer />
 </template>
 
@@ -24,24 +15,10 @@
 import Header from '@/components/SiteHeader.vue'
 import Footer from '@/components/SiteFooter.vue'
 import { ref, onMounted } from 'vue'
+import ProfileCard from '@/components/ProfileCard.vue'
 import { supabase } from '@/supabase'
+import type TeamMember from '@/types/teamMember'
 
-try {
-  let { data: team, error } = await supabase
-  .from('team')
-  .select('*')
-  console.log(team);  
-  if (error) throw error
-} catch (error) {
-  console.error(error);
-}
-interface TeamMember {
-  name: string
-  role: string
-  image: string
-  github_link: string
-  linkedin_link: string
-}
 
 const team = ref<TeamMember[]>([])
 
@@ -54,6 +31,8 @@ onMounted(async () => {
     console.error(error)
   }
 })
+
+
 </script>
 
 <style lang="css" scoped>
@@ -74,41 +53,7 @@ color: white;
       
   }
 
-  .team-wrapper {
-      flex: 1 1 300px;
-      margin: 10px;
-      text-align: left;
-      border: 1px solid #524b4b;
-      padding: 30px;
-      font-family: Arial, sans-serif;
-  }
 
-  .team-wrapper h3 {
-      color: white;
-      font-size: 24px;
-      font-weight: bold;
-      padding-top: 10px;
-  }
-
-  .team-wrapper h4 {
-      color: #d54ab6;
-      font-size: 18px;
-      font-weight: normal;
-      padding-top: 10px;
-  }
-
-  .team-wrapper p {
-      color: #888;
-      font-size: 16px;
-      padding-top: 10px;
-     
-  }
-
-  .team-wrapper a {
-     color: #337ab7;
-      text-decoration: none;
-      padding-top: 10px;
-  }
   @media (max-width: 1100px) {
   .container {
     padding: 20px;
