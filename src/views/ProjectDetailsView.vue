014031c3deb1bf435399c9a2125de89b2de104fa
amarinder1029 <109933668+amarinder1029@users.noreply.github.com>
2023-05-09 01:01:06 -0700

    added some tech Stack items

--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -1,12 +1,50 @@
 <template>
   <Header />
-  <h1>We'd love to hear from you</h1>
-  <button @click="toggleOk"> Proceed</button>
-  <template v-if="!ok">
-    <h1>Title</h1>
-    <p>Paragraph 1</p>
-    <p>Paragraph 2</p>
-  </template>
+  <h1 class="title">Tech Stack</h1>
+ 
+
+  <div class="about">
+		<div class="container">
+			<div class="row">
+				<div class="image">
+					<img src="@/assets/Images/Vue.js_Logo_2.svg.png" class="img-responsive" alt="about img">
+				</div>
+
+				<div class="description">
+					<h3>Front End</h3>
+					<p>the project front end was implemented using the <a href="https://angular.io/">Vue</a>  framework and <a href="https://dummyjson.com/">Dummtyjson</a> API </p>
+				</div>
+        
+			</div>
+			<div class="row">
+
+				<div class="image">
+          <img src="@/assets/Images/supabase.jpg" class="img-responsive" alt="about img">
+				</div>
+        
+				<div class="description">
+					<h3>Back End</h3>
+					<p>the team decided to use the Google suite of products offered though <a href="https://supabase.com/docs"> Supabase</a>
+						This includes: 
+					</p>
+					<ul>
+						<li>
+							Authentication
+						</li>
+						<li>
+							cloud storage
+						</li>
+					
+					</ul>
+					
+				
+				</div>
+			</div>
+		</div>
+	</div> 
+
+
+
   <Footer />
 </template>
   
@@ -14,12 +52,49 @@
 import Header from '@/components/SiteHeader.vue'
 import Footer from '@/components/SiteFooter.vue'
 import { ref } from 'vue';
-let ok = ref(true);
 
-function toggleOk() {
-  ok.value = !ok.value
-}
+
 </script>
+
+<style lang="css" scoped>
+.title {
+  text-align: center;
+}
+
+.img-responsive {
+  height: 200px;
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+ 
+  padding: 50px;
+ 
+}
+
+.row > .image,
+.row > .description {
+  flex: 1; /* Equal width for both image and description */
   
-<style lang="scss" scoped></style>
-  
+}
+
+.description {
+  text-align: start;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column; /* Stack rows vertically */
+}
+.image
+{
+ text-align: center;
+}
+.description
+{
+padding-right: 100px;
+}
+
+</style>
