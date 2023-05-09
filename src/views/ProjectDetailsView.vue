48c6e984d935a13f5dc2cf6a0344dbca1228e6f5
amarinder1029 <109933668+amarinder1029@users.noreply.github.com>
2023-05-09 15:42:42 -0700

    fully responsive project details page

--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -4,7 +4,7 @@
  
 
   <div class="about">
-		<div class="container">
+		<div class="tech-stack">
 			<div class="row">
 				<div class="image">
 					<img src="@/assets/Images/Vue.js_Logo_2.svg.png" class="img-responsive" alt="about img">
@@ -12,7 +12,7 @@
 
 				<div class="description">
 					<h3>Front End</h3>
-					<p>the project front end was implemented using the <a href="https://angular.io/">Vue</a>  framework and <a href="https://dummyjson.com/">Dummtyjson</a> API </p>
+					<p>the project front end was implemented using the <a href="https://angular.io/">Vue</a>  framework and <a href="https://dummyjson.com/">Dummyjson</a> API </p>
 				</div>
         
 			</div>
@@ -41,6 +41,22 @@
 				</div>
 			</div>
 		</div>
+		<div class="data">
+			<h1 class="title ">Data Modeling</h1>
+			<p> Data modeling is the process of creating a conceptual representation of data and its relationships to support efficient storage, retrieval, and manipulation.</p>
+			<div class="image-data">
+				<img src="@/assets/Images/db.png" class="data-pic" alt="about img">
+			</div>
+		</div>
+		<div class="Design">
+			<div class="">
+			<h1 class="title">Site design </h1>
+			<p> site description</p>
+		</div>
+			<div class="image-data">
+				<img src="@/assets/Images/siteDiagram.png" class="site-pic" alt="about img">
+			</div>
+		</div>
 	</div> 
 
 
@@ -62,14 +78,14 @@ import { ref } from 'vue';
 }
 
 .img-responsive {
-  height: 200px;
+  height: 300px;
 }
 
 .row {
   display: flex;
   justify-content: center;
  
-  padding: 50px;
+
  
 }
 
@@ -81,20 +97,80 @@ import { ref } from 'vue';
 
 .description {
   text-align: start;
+  background-color: rgb(48, 46, 46);
+  padding: 20px;
 }
 
-.container {
+.tech-stack {
   display: flex;
   justify-content: center;
   flex-direction: column; /* Stack rows vertically */
+  padding: 50px;
 }
-.image
+.image-data
 {
  text-align: center;
 }
 .description
 {
 padding-right: 100px;
+}
+.data-pic
+{
+	padding: 50px 0px;
+	height: 900px;
+	width: 1150px;
+}
+.site-pic
+{
+	padding: 50px 0px;
+	height: 1300px;
+	width: 1150px;
+}
+p
+{
+	padding-left: 20px;;
+	font-size: 20px;
+}
+
+@media (max-width: 1150px) {
+  .row {
+    flex-direction: column;
+	
+  }
+
+  .description {
+    padding-right: 0; 
+  }
+
+  .data-pic {
+    height: auto; 
+    width: 100%; 
+  }
+
+  .site-pic {
+    height: auto; 
+    width: 100%;
+  }
+
+  p {
+    padding-left: 0; 
+    font-size: 18px; 
+  }
+  .image
+  {
+	text-align: center;
+  }
+}
+@media (max-width: 470px) {
+  .row {
+    flex-direction: column;
+	padding: 0px 0px;
+  }
+  .img-responsive
+  {
+	height: 200px;
+  }
 }
 
 </style>
