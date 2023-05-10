55d2ebc28fe4d357ceaa95929259c4bf4bfe3f39
KaroKsach <35212646+KaroKsach@users.noreply.github.com>
2023-05-10 11:09:39 -0700

    Creates cart, stores data locally on user machine

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,12 +1,67 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 import type Product from '../types/product'
-import {ref} from 'vue'
+import { ref } from 'vue'
 
-export const cartStore = defineStore('cart',()=>{
-    const cart = ref<Product[]>([]);
-    function addToCart(){ }
+interface CartItem {
+  product: Product
+  quantity: number
+}
 
-    function deleteFromCart(){}
-    
-    return{cart, addToCart, deleteFromCart}
+export const useCartStore = defineStore('cart', () => {
+  const cart = ref<CartItem[]>([])
+
+  // retrieve cart from localStorage
+  const savedCart = localStorage.getItem('cart')
+  if (savedCart) {
+    cart.value = JSON.parse(savedCart)
+  }
+
+  function addToCart(product: Product) {
+    const existingItem = cart.value.find((item) => item.product.id === product.id)
+    if (existingItem) {
+      existingItem.quantity++
+    } else {
+      cart.value.push({ product, quantity: 1 })
+    }
+
+    // store cart in localStorage
+    localStorage.setItem('cart', JSON.stringify(cart.value))
+    console.log(cart.value)
+    console.log('Cart contents:')
+    cart.value.forEach((item) => {
+      console.log(`${item.product.title}: ${item.quantity}`)
+    })
+  }
+
+  function removeFromCart(product: Product) {
+    const existingItem = cart.value.find((item) => item.product.id === product.id)
+    if (existingItem) {
+      if (existingItem.quantity > 1) {
+        existingItem.quantity--
+      } else {
+        deleteFromCart(product)
+      }
+    }
+  }
+  function deleteFromCart(product: Product) {
+    const index = cart.value.findIndex((item) => item.product.id === product.id)
+    if (index !== -1) {
+      cart.value.splice(index, 1)
+    }
+
+    // store cart in localStorage
+    localStorage.setItem('cart', JSON.stringify(cart.value))
+  }
+
+  function updateQuantity(product: Product, quantity: number) {
+    const item = cart.value.find((item) => item.product.id === product.id)
+    if (item) {
+      item.quantity = quantity
+
+      // store cart in localStorage
+      localStorage.setItem('cart', JSON.stringify(cart.value))
+    }
+  }
+
+  return { cart, addToCart, deleteFromCart, removeFromCart, updateQuantity }
 })
--- src/views/ProductView/index.vue
+++ src/views/ProductView/index.vue
@@ -1,151 +1,153 @@
 <template>
-    <Header />
-    <main>
-        <div class="image-widget-wrapper">
-            <ImageWidget :images="images" />
-        </div>
-        <div class="product-details-wrapper">
-            <h2>{{ product.title }}</h2>
-            <p>{{ product.description }}</p>
-            <p>buy new: </p>
-            <p> <span class="dollar">$</span>{{ product.price }}</p>
-            <p>first time buyer discount:</p>
-            <p><span class="dollar">$</span>{{ discountedPrice }}</p>
-            <p>average rating: {{ product.rating }} <span class="web-gold"><font-awesome-icon
-                        :icon="['fas', 'star']" /></span></p>
-            <div class="btn-container">
-                <button class="azure">add to cart</button>
-                <button class="rus-violet">buy now</button>
-            </div>
-        </div>
-    </main>
-    <Footer />
+  <Header />
+  <main>
+    <div class="image-widget-wrapper">
+      <ImageWidget :images="images" />
+    </div>
+    <div class="product-details-wrapper">
+      <h2>{{ product.title }}</h2>
+      <p>{{ product.description }}</p>
+      <p>buy new:</p>
+      <p><span class="dollar">$</span>{{ product.price }}</p>
+      <p>first time buyer discount:</p>
+      <p><span class="dollar">$</span>{{ discountedPrice }}</p>
+      <p>
+        average rating: {{ product.rating }}
+        <span class="web-gold"><font-awesome-icon :icon="['fas', 'star']" /></span>
+      </p>
+      <div class="btn-container">
+        <button class="azure" @click="addToCart(product)">add to cart</button>
+        <button class="rus-violet">buy now</button>
+      </div>
+    </div>
+  </main>
+  <Footer />
 </template>
-    
+
 <script lang="ts" setup>
 import Header from '@/components/SiteHeader.vue'
 import Footer from '@/components/SiteFooter.vue'
-import ImageWidget from './ImageWidget.vue';
-import type Product from '@/types/product';
-import { useRoute } from 'vue-router';
-import { computed, reactive } from 'vue';
-import { baseURL } from '@/endpoint';
-
+import ImageWidget from './ImageWidget.vue'
+import type Product from '@/types/product'
+import { useRoute } from 'vue-router'
+import { computed, reactive } from 'vue'
+import { baseURL } from '@/endpoint'
+import { useCartStore } from '@/stores/cart'
 
 const route = useRoute()
 // need to cast the paramater; this caused such a god damn fit
-let id: number = parseInt(<string>route.params.id);
+let id: number = parseInt(<string>route.params.id)
 
 // use generics to propery type the reactive obj
-let product: Product = reactive(<Product>{});
+let product: Product = reactive(<Product>{})
 
-    
 try {
-    const response = await fetch(`${baseURL}/${id}`);
-    product = await response.json();
+  const response = await fetch(`${baseURL}/${id}`)
+  product = await response.json()
 } catch (error) {
-    console.log(error);
+  console.log(error)
 }
 
-let images: string[] = product.images;
-console.log(product);
+let images: string[] = product.images
+console.log(product)
 
 const discountedPrice = computed(() => {
-    return Math.floor(product.price - ((product.discountPercentage / 100) * product.price))
+  return Math.floor(product.price - (product.discountPercentage / 100) * product.price)
 })
 
+const cartStore = useCartStore()
+
+const addToCart = (product: Product) => {
+  cartStore.addToCart(product)
+}
 </script>
-    
+
 <style lang="css" scoped>
 p,
 span {
-    font-size: 1.25rem;
+  font-size: 1.25rem;
 }
 
 h2 {
-    color: var(--brilliant-azure);
+  color: var(--brilliant-azure);
 }
 
 .btn-container {
-    margin-top: 1em;
-    display: flex;
-    flex-direction: column;
-    gap: 1em;
+  margin-top: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
 }
 
 button {
-    border-radius: .33rem;
-    border: transparent;
-    color: var(--color-text);
-    font-size: 1.25rem;
-    width: 40%;
-    padding: 1.25em auto;
+  border-radius: 0.33rem;
+  border: transparent;
+  color: var(--color-text);
+  font-size: 1.25rem;
+  width: 40%;
+  padding: 1.25em auto;
 }
 
 button:hover {
-    border-bottom: 2px solid var(--brilliant-azure);
+  border-bottom: 2px solid var(--brilliant-azure);
 }
 
-
 .azure {
-    background-color: var(--persian-blue);
+  background-color: var(--persian-blue);
 }
 
 .web-gold {
-    color: var(--web-gold);
+  color: var(--web-gold);
 }
 
 .rus-violet {
-    background-color: var(--russian-violet);
+  background-color: var(--russian-violet);
 }
 
 main {
-    /* border: 2px solid blue; */
-    display: grid;
-    max-width: 1280px;
-    gap: 1em;
-    grid-auto-flow: row;
-    margin: 2em auto;
+  /* border: 2px solid blue; */
+  display: grid;
+  max-width: 1280px;
+  gap: 1em;
+  grid-auto-flow: row;
+  margin: 2em auto;
 }
 
 .product-details-wrapper {
-    border-bottom: 2px solid var(--color-border);
-    border-left: 2px solid var(--color-border);
-    padding-left: 1em;
-    padding-bottom: 2em;
+  border-bottom: 2px solid var(--color-border);
+  border-left: 2px solid var(--color-border);
+  padding-left: 1em;
+  padding-bottom: 2em;
 }
 
 .image-widget-wrapper {
-    border-bottom: 2px solid var(--persian-rose-dull);
-    padding-bottom: 2em;
-
+  border-bottom: 2px solid var(--persian-rose-dull);
+  padding-bottom: 2em;
 }
 
 .dollar {
-    color: var(--electric-pink);
+  color: var(--electric-pink);
 }
 
 /* tablet */
 @media only screen and (min-width: 631px) {
-    main {
-        grid-template-columns: repeat(2, 1fr);
-    }
-
+  main {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 
 /* laptop screens  */
 @media only screen and (min-width: 1081px) {
-    main {
-        grid-template-columns: repeat(3, 1fr);
-    }
+  main {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
-    .product-details-wrapper {
-        grid-column: 3/4;
+  .product-details-wrapper {
+    grid-column: 3/4;
+  }
 
-    }
-
-    .image-widget-wrapper {
-        grid-column: span 2;
-    }
-}</style>
-    
+  .image-widget-wrapper {
+    grid-column: span 2;
+  }
+}
+</style>
