0e1880a6640362e46ce67c9769ecd3ef6af3f782
Brian <bbarr0101@gmail.com>
2023-05-09 23:16:53 -0700

    add form components specifically for checkout page

---
+++ src/views/CheckoutView/AddressForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <form action="">
+        <BaseInput 
+        label="street"
+        v-model="street"
+        />
+    </form>        
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import BaseInput from '@/components/BaseInput.vue';
+
+const address = {
+    street: ref(''),
+    city: ref(''),
+    zip: ref(''),
+    state: ref('')
+}
+const {street, city, zip , state} = address;
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
