92da6d3ae16633e05a720c1d4528a29455c2e3a5
Brian <bbarr0101@gmail.com>
2023-05-11 15:44:44 -0700

    fixes rendering of the list

--- src/views/CheckoutView/index.vue
+++ src/views/CheckoutView/index.vue
@@ -2,7 +2,7 @@
     <Header />
     <main class="grid-wrapper">
         <div class="cart-list">
-            <CartItem v-for="cartItem in cart" :item="cartItem" />
+            <CartItem v-for="cartItem in cart" :item="cartItem" :key="cartItem.product.id" />
         </div>
         <div class="form-area">
             <AddressForm />
